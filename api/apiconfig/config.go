77ec60c94b8d9c9d7eaea8a5260cb0f7aac0c771
Vivekanantha <Vivekananthachari.g@indiumsoft.com>
2022-04-25 11:42:48 +0530

    push
    push

--- api/apiconfig/config.go
+++ api/apiconfig/config.go
@@ -5,12 +5,8 @@ var apimain string
 func API(apinew string) (val string) {
 
 	//apimain = "http://35.83.46.51:3000/"
-	apimain = "http://127.0.0.1:3000/"
-<<<<<<< HEAD
-	// apimain = "http://52.39.98.71:3000/"
-=======
-	//apimain = "http://52.39.98.71:3000/"
->>>>>>> e6b424662d78f2fe9fe106a7f60e8272c5e8b814
+	//	apimain = "http://127.0.0.1:3000/"
+	apimain = "http://52.39.98.71:3000/"
 
 	return apimain + apinew
 }
