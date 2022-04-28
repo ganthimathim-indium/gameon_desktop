e05dca74f022ca79709b6602429c8d9e63e14987
Vivekanantha <Vivekananthachari.g@indiumsoft.com>
2022-04-28 13:44:52 +0530

    code refactor
    code refactor

--- api/apiconfig/config.go
+++ api/apiconfig/config.go
@@ -1,12 +1,12 @@
 package apiconfig
 
-var apimain string
+var apiMain string
 
-func API(apinew string) (val string) {
+func API(apiNew string) (val string) {
 
-	//apimain = "http://35.83.46.51:3000/"
-	// apimain = "http://127.0.0.1:3000/"
-	apimain = "http://52.39.98.71:3000/"
+	//apiMain = "http://35.83.46.51:3000/"
+	//apiMain = "http://127.0.0.1:3000/"
+	apiMain = "http://52.39.98.71:3000/"
 
-	return apimain + apinew
+	return apiMain + apiNew
 }
